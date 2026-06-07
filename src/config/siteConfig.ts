56717b36a01d4f2a4c7f4ee2d4082f791fba8163
Kabuto32324 <a2709928366@gmail.com>
2026-06-07 22:17:04 +0800

    fix: 启用 51la autoTrack 并重新构建

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -215,7 +215,7 @@ export const siteConfig: SiteConfig = {
 			// 多个统计 ID 的数据分离标识，留空则使用 Id
 			ck: "",
 			// 是否开启事件分析功能
-			autoTrack: false,
+			autoTrack: true,
 			//  Hash路由模式, 项目使用History API路由, 所以不必开启默认false
 			hashMode: false,
 			// 是否开启网站录屏功能
